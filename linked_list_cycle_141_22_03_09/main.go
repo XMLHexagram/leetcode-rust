76f48798eefa4e23d9b30c2811e2ca07541a2292
hexagram <xml.hexagram@gmail.com>
2022-03-09 00:41:15 +0800

    linked_list_cycle_141_22_03_09

---
+++ linked_list_cycle_141_22_03_09/main.go
@@ -0,0 +1,16 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	for i := 0; i < 10*10*10*10+5; i++ {
+		if head == nil {
+			return false
+		}
+		head = head.Next
+	}
+	return true
+}
